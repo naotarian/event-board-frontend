b39cfec27c307ff99c573057723aaa1cedaad1bd
naotarian <naotarian19@gmail.com>
2022-05-10 08:08:59 +0000

    ログインリンクとか諸々

---
+++ src/components/Parts/Template/Header.js
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import styled from "styled-components"
+import Grid from '@mui/material/Grid'
+import AppBar from '@mui/material/AppBar'
+import Link from 'next/link'
+import { useAuth } from '@/hooks/auth'
+import HeaderCss from '../../../../styles/Parts/Template/Header.module.css'
+
+const StyledAppBar = styled(AppBar)`
+  padding: 1rem;
+  box-shadow: 0 2px 5px rgb(0 0 0 / 10%);
+  height: 60px;
+  position: fixed;
+  top: 0;
+  background-color: #fff;
+`
+const Header = () => {
+  const { user } = useAuth({ middleware: 'guest' })
+  const { logout } = useAuth()
+  return (
+    <StyledAppBar>
+      {/* <Grid>Header</Grid> */}
+      <div className={`fixed top-0 right-0 px-6 py-4 sm:block`}>
+        {user ?
+          <div>
+            <Link href="/dashboard">
+              <a className={`ml-4 text-sm ${HeaderCss.headerLink}`}>
+                Dashboard
+              </a>
+            </Link>
+            {/* <Link href={logout}> */}
+            <a className={`ml-4 text-sm ${HeaderCss.headerLink}`} onClick={logout}>
+              ログアウト
+            </a>
+            {/* </Link> */}
+          </div>
+          :
+          <>
+            <Link href="/login">
+              <a className={`ml-4 text-sm ${HeaderCss.headerLink}`}>Login</a>
+            </Link>
+
+            <Link href="/register">
+              <a className={`ml-4 text-sm ${HeaderCss.headerLink}`}>
+                Register
+              </a>
+            </Link>
+          </>
+        }
+      </div>
+    </StyledAppBar>
+
+  )
+}
+export default Header
