5167358afbd50ce4a1b24613ec8900dee848d97c
naotarian <naotarian19@gmail.com>
2022-05-18 07:49:51 +0000

    once up

--- src/components/Parts/ResultList/TagModal.js
+++ src/components/Parts/ResultList/TagModal.js
@@ -1,21 +1,64 @@
 import { useState } from 'react'
+import styled from "styled-components"
+//mui
 import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-const style = {
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+const modalStyle = {
   position: 'absolute',
-  top: '50%',
+  top: '20%',
   left: '50%',
+  width: 900,
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
+const ModalBox = styled(Box)`
+  overflow-y: scroll;
+`
+const TagTitle = styled(Typography)`
+  margin-bottom: 15px;
+  color: #253458;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 700;
+`
+const TagBand = styled(Typography)`
+  padding: 6px 20px;
+  margin-bottom: 15px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 2px;
+  background-color: #333;
+`
 const TagModal = (props) => {
   const { tagModal, tagModalClose } = props
+  function FormRow() {
+    return (
+      <>
+        <Grid item xs={3}>
+          <Item>.NET</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>ActionScript</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>Agda</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>Akka</Item>
+        </Grid>
+      </>
+    );
+  }
+  const Item = styled(Grid)`
+    padding: 1rem;
+  `
   return (
     <Modal
       open={tagModal}
@@ -23,15 +66,22 @@ const TagModal = (props) => {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+      <ModalBox sx={modalStyle}>
+        <TagTitle id="modal-modal-title">
+          タグを選択
+        </TagTitle>
+        <TagBand id="modal-modal-description" sx={{ mt: 2 }}>
+          プログラム
+        </TagBand>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={1}>
+            <Grid container item spacing={3}>
+              <FormRow />
+            </Grid>
+          </Grid>
+        </Box>
         <Button onClick={tagModalClose}>閉じる</Button>
-      </Box>
+      </ModalBox>
     </Modal>
   )
 }
