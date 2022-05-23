a5e08f2e83e78d83413aa579edee218db1d77516
naotarian <naotarian19@gmail.com>
2022-05-23 10:42:40 +0000

    sidebar分割component

---
+++ src/components/Parts/ResultList/SideBarSearchArea.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+//mui
+import Paper from '@mui/material/Paper'
+import styled from "styled-components"
+import Grid from '@mui/material/Grid'
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
+import CheckBoxIcon from '@mui/icons-material/CheckBox'
+//components
+import SideBarTagSearch from './SideBarTagSearch'
+import SideBarSearch from './SideBarSearch'
+//css
+const SideBar = styled(Paper)`
+  padding: 1.5rem;
+  margin-bottom: 2rem;
+`
+const SideBarWrapper = styled(Grid)`
+  width: 35%;
+  position: fixed;
+  right: 36px;
+`
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />
+const checkedIcon = <CheckBoxIcon fontSize="small" />
+const SideBarSearchArea = (props) => {
+  const [selectDateStart, setSelectDateStart] = useState(null)
+  const [selectDateEnd, setSelectDateEnd] = useState(null)
+  const { TagFocus } = props
+  return (
+    <SideBarWrapper>
+      <SideBar>
+        <SideBarSearch TagFocus={TagFocus} />
+      </SideBar>
+      <SideBar>
+        <SideBarTagSearch />
+      </SideBar>
+    </SideBarWrapper>
+  )
+}
+export default SideBarSearchArea
