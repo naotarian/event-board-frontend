63ea6cdaab8124d85ac808052b0263e234724ee8
naotarian <naotarian19@gmail.com>
2022-07-01 11:26:01 +0000

    lint設定

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  // distDir: 'src/build'
-};
-// const nextConfig = {
-//   reactStrictMode: true,
-//   // compiler: {
-//   //   styledComponents: true,
-//   // },
-// }
-
-// module.exports = nextConfig
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
